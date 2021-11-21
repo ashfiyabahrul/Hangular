d25106e0724bfd3ea5135b84c7803bfbaaa2dbf9
mhikal45 <a2292218@bangkit.academy>
2021-11-21 14:18:20 +0700

    adding fragment

--- app/src/main/java/com/hangular/hangular/MainActivity.kt
+++ app/src/main/java/com/hangular/hangular/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        actionBar?.hide()
+        supportActionBar?.hide()
 
         if (fragment !is MenuUtamaFragment) {
             mFragmentManager
